262beb97b906fa16c44d5c898575ab3efd77bbee
unknown <davi.cesarbt@gmail.com>
2023-06-23 16:08:41 -0300

    exercise 18 made

---
+++ how-would-I-make-a-repo.js
@@ -0,0 +1,10 @@
+/* 
+1. git clone [URL-do-repo-remoto] : clonar repositório
+2. git init : inicio nosso repo local 
+3. git branch [nome-da-branch] : Criar branch
+    3.a git switch [nome-da-branch] : navegar para branch criada
+4. git add [ . ] ou [nome-do-arquivo] : rastrear mudanças
+5. git commit -m “[mensagem-muito-bem-escrita]” : salvar permanentemente no repo local
+6. git push : salvar permanentemente no repo remoto
+    6.a git push --set-upstream origin [exemplo-error-remoto]
+*/
